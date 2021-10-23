927b0bf9a53e2585bd16a375922665d5551d88fd
CodeHime <krima282@gmail.com>
2021-10-23 07:00:16 -0700

    Adding wrapper function

--- Code/src/similarity_matrizer.py
+++ Code/src/similarity_matrizer.py
@@ -1,5 +1,15 @@
 from distance_calculator import *
 
+def perform_dimensionality_reductions(matrix, k, technique):
+  if technique=="pca":
+    obj = Pca(k,matrix)
+  elif technique=="svd":
+    obj = Svd(k,matrix)
+  else:
+    obj = Lda(k,matrix)
+  obj.save("")
+  return obj.get_obj_weight_pairs()
+
 def get_similarity_matrix(xb, technique="pca", method="euclidean"):
     """
     Get similarity matrix of features
