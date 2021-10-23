cf30bcbeaf980db6684ff48b22264c0c6dd0b682
CodeHime <krima282@gmail.com>
2021-10-23 07:01:54 -0700

    Adding wrapper function

--- Code/src/similarity_matrizer.py
+++ Code/src/similarity_matrizer.py
@@ -14,7 +14,7 @@ def perform_dimensionality_reductions(matrix, k, technique):
   obj.save("")
   return obj.get_obj_weight_pairs()
 
-def get_similarity_matrix(xb, technique="pca", method="euclidean"):
+def get_similarity_matrix(xb, k, technique="pca", method="euclidean"):
     """
     Get similarity matrix of features
 
@@ -23,5 +23,6 @@ def get_similarity_matrix(xb, technique="pca", method="euclidean"):
     for xq in xb:
         similarity_matrix.append(euclidean_fn(xb, xb.shape[0], xq))
     # Perform technique and return top k latent semantics
+    sub_wt_pairs = perform_dimensionality_reductions(similarity_matrix, k, technique)
 
     return np.array(similarity_matrix)
